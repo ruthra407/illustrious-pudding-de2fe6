3f0571b1211c42f4f09d477858902b77a6524c61
ruthra407 <ruthra407@gmail.com>
2026-08-17 22:45:00 +0530

    Create BootReceiver.java

---
+++ android/app/src/main/java/com/newfashion/tailoring/BootReceiver.java
@@ -0,0 +1,17 @@
+package com.newfashion.tailoring;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+
+            ReminderScheduler.rescheduleAll(context);
+        }
+    }
+}
